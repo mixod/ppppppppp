f548d80b0fa9cbfcce42e8053d813c5f0790aba6
Sangram Thapa <thapasangram64@gmail.com>
2024-12-29 16:34:27 +0545

    pp

--- practice/src/Calc.jsx
+++ practice/src/Calc.jsx
@@ -12,15 +12,18 @@ const green = () =>{
   setColor("green");
 }
 
-  return (
+  return (  
     <>
     <h1>My favourite color is {color} </h1>
-
+    <button onClick={()=> setColor("Black")}>black</button>
+    <button onClick={()=> setColor("white")}>white</button>
+    <button onClick={()=> setColor("pink")}>pink</button>
     <button onClick={red}>red</button>
     <button onClick={green}>green</button>
     <button onClick={()=> setColor("yellow")}>yellow</button>
+ 
     </>
-  )
+  ) 
 }
 
 export default Calc
