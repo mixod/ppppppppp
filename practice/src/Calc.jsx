f56c1ced4d7aa184a5ca927d875a5efb23b1b37c
Sangram Thapa <thapasangram64@gmail.com>
2025-01-01 12:18:18 +0545

    hello

--- practice/src/Calc.jsx
+++ practice/src/Calc.jsx
@@ -1,48 +1,88 @@
 // import { useState } from "react"
 
-import { useState } from "react"
-
 // import { useState } from "react"
 
 
-// function Calc() {
-//   const [state,setState]=useState(" alu");
 
-//     const change=()=>{
-//       setState("kalu");
-//     }
+// // function Calc() {
+// //   const [state,setState]=useState(" alu");
+
+// //     const change=()=>{
+// //       setState("kalu");
+// //     }
   
 
 
-//   return (
-//     <>
-//     <div>{state}</div>
-//    <button onClick={change}>click</button>
-//    </>
-//   )
-// }
+// //   return (
+// //     <>
+// //     <div>{state}</div>
+// //    <button onClick={change}>click</button>
+// //    </>
+// //   )
+// // }
 
-// export default Calc
+// // export default Calc
+
+
+
+
+// // function Calc() {
+// //   const [color,setColor]=useState(" ");
+// //   const yellow=()=>{
+// //     setColor("yellow");
+// //   }
+
+// //   return (
+// //    <>
+// //    <h2>My favourite color is {color}</h2>
+
+// //    <button onClick={()=>setColor("red")}>RED</button>
+// //    <button onClick={yellow}>Yellow</button>
+
+   
+   
+// //    </>
+// //   )
+// // }
+
+// // export default Calc
+
+
+
+
 
 
 
 
 // function Calc() {
-//   const [color,setColor]=useState(" ");
-//   const yellow=()=>{
-//     setColor("yellow");
-//   }
+
+//   const [state, setState]=useState(" ");
+//   const [value1, setValue1]=useState(" ");
+//   const [value2, setValue2]=useState(" ");
+
+// const add=()=>{
+//    const result=value1+value2;
+//    setState(result);
+// }
+
+// const firstnum=(e)=>{
+//   const a=parseInt(e.target.value);
+//   setValue1(a);
+// }
+
+// const secondnum=(e)=>{
+//   const b=parseInt(e.target.value);
+//   setValue2(b);
+// }
 
 //   return (
-//    <>
-//    <h2>My favourite color is {color}</h2>
+//     <>
+//   <input type="number" onChange={firstnum}></input>
+//   <input type="number"onChange={secondnum}></input>
+//   <button onClick={add}>ADD</button>
+//   <div>Rsult{state}</div>
 
-//    <button onClick={()=>setColor("red")}>RED</button>
-//    <button onClick={yellow}>Yellow</button>
-
-   
-   
-//    </>
+//     </>
 //   )
 // }
 
@@ -51,38 +91,29 @@ import { useState } from "react"
 
 
 
-
-
-
+import { useState } from "react"
 
 function Calc() {
+  const initstae="thid is home page"
 
-  const [state, setState]=useState(" ");
-  const [value1, setValue1]=useState(" ");
-  const [value2, setValue2]=useState(" ");
+  const [state,setState]=useState(initstae);
 
-const add=()=>{
-   const result=value1+value2;
-   setState(result);
-}
 
-const firstnum=(e)=>{
-  const a=parseInt(e.target.value);
-  setValue1(a);
-}
-
-const secondnum=(e)=>{
-  const b=parseInt(e.target.value);
-  setValue2(b);
-}
-
+  const changePage=()=>{
+    setState("this is another page");
+  }
+  const reset=()=>{
+    setState(initstae);
+  }
   return (
     <>
-  <input type="number" onChange={firstnum}></input>
-  <input type="number"onChange={secondnum}></input>
-  <button onClick={add}>ADD</button>
-  <div>Rsult{state}</div>
-
+    <div>{state}</div>
+    <button onClick={changePage}>click for net page</button>
+    {state === "this is another page" &&(
+      <button onClick={reset}>click</button>
+    )
+    }
+    
     </>
   )
 }
