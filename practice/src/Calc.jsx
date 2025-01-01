9771da1019ec559e0118d5020f1ada8b1e242c7f
Sangram Thapa <thapasangram64@gmail.com>
2025-01-01 11:12:30 +0545

    practice usestate in color

--- practice/src/Calc.jsx
+++ practice/src/Calc.jsx
@@ -1,19 +1,42 @@
+// import { useState } from "react"
+
 import { useState } from "react"
 
 
-function Calc() {
-  const [state,setState]=useState(" alu");
+// function Calc() {
+//   const [state,setState]=useState(" alu");
 
-    const change=()=>{
-      setState("kalu");
-    }
+//     const change=()=>{
+//       setState("kalu");
+//     }
   
 
 
+//   return (
+//     <>
+//     <div>{state}</div>
+//    <button onClick={change}>click</button>
+//    </>
+//   )
+// }
+
+// export default Calc
+
+
+
+
+function Calc() {
+  const [color,setColor]=useState(" ");
+
+
   return (
-    <>
-    <div>{state}</div>
-   <button onClick={change}>click</button>
+   <>
+   <h2>My favourite color is {color}</h2>
+
+   <button onClick={()=>setColor("red")}>RED</button>
+
+   
+   
    </>
   )
 }
