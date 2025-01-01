fc9000b1681a94da0919db6aae1e37060b264b78
Sangram Thapa <thapasangram64@gmail.com>
2025-01-01 11:27:59 +0545

    hello

--- practice/src/Calc.jsx
+++ practice/src/Calc.jsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react"
 
+// import { useState } from "react"
+
 
 // function Calc() {
 //   const [state,setState]=useState(" alu");
@@ -25,22 +27,63 @@ import { useState } from "react"
 
 
 
+// function Calc() {
+//   const [color,setColor]=useState(" ");
+//   const yellow=()=>{
+//     setColor("yellow");
+//   }
+
+//   return (
+//    <>
+//    <h2>My favourite color is {color}</h2>
+
+//    <button onClick={()=>setColor("red")}>RED</button>
+//    <button onClick={yellow}>Yellow</button>
+
+   
+   
+//    </>
+//   )
+// }
+
+// export default Calc
+
+
+
+
+
+
+
+
 function Calc() {
-  const [color,setColor]=useState(" ");
-  const yellow=()=>{
-    setColor("yellow");
-  }
+
+  const [state, setState]=useState(" ");
+  const [value1, setValue1]=useState(" ");
+  const [value2, setValue2]=useState(" ");
+
+const add=()=>{
+   const result=value1+value2;
+   setState(result);
+}
+
+const firstnum=(e)=>{
+  const a=parseInt(e.target.value);
+  setValue1(a);
+}
+
+const secondnum=(e)=>{
+  const b=parseInt(e.target.value);
+  setValue2(b);
+}
 
   return (
-   <>
-   <h2>My favourite color is {color}</h2>
+    <>
+  <input type="number" onChange={firstnum}></input>
+  <input type="number"onChange={secondnum}></input>
+  <button onClick={add}>ADD</button>
+  <div>Rsult{state}</div>
 
-   <button onClick={()=>setColor("red")}>RED</button>
-   <button onClick={yellow}>Yellow</button>
-
-   
-   
-   </>
+    </>
   )
 }
 
