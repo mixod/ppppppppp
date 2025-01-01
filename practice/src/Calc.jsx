116c8e9b2ffd274aa0b375cfbef3c9946e64a2be
Sangram Thapa <thapasangram64@gmail.com>
2025-01-01 11:08:24 +0545

    ppp

--- practice/src/Calc.jsx
+++ practice/src/Calc.jsx
@@ -1,61 +1,20 @@
-// import { useState } from "react"
-
 import { useState } from "react"
 
 
-
-
-// function Calc() {
-//   const [state,setState]= useState();
-//   const [value1,setValue1]= useState();
-//   const [value2,setValue2]= useState();
-
-//   function add(){
-//     const result=value1+value2;
-//     setState(result);
-//   }
-
-//    function firstnumber(e){
-//     const result=parseInt(e.target.value);
-//     setValue1(result);
-//    }
-//    function secondnumber(e){
-//     const result=parseInt(e.target.value);
-//     setValue2(result);
-//    }
-  
-  
-//   return (
-//     <>
-//     <input type="number" onChange={firstnumber}></input>
-//     <input type="number" onChange={secondnumber}></input>
-//     <button onClick={add}>Add</button>
-//     <div>result:{state} </div>
-
-//     </>
-//   )
-// }
-
-// export default Calc
-
-
-
-
-
 function Calc() {
+  const [state,setState]=useState(" alu");
 
-const [state,setState]=useState("hari");
+    const change=()=>{
+      setState("kalu");
+    }
+  
 
-  const neww =()=>{
-    setState("shyam");
-  }
 
   return (
     <>
-    
-    <button onClick={neww}>click</button>
     <div>{state}</div>
-    </>
+   <button onClick={change}>click</button>
+   </>
   )
 }
 
